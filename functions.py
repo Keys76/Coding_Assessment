d3b16efe3f71680f82ec30f1a650b58b61cabac4
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-06 16:04:14 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,11 +2,10 @@ import os
 import sys
 import time
 
+# Function to clear the console screen
 def clear_screen():
     os.system ('cls' if os.name == 'nt' else 'clear')
 
-def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 def animate(text, delay=0.05):
     for c in text:
@@ -15,6 +14,7 @@ def animate(text, delay=0.05):
         time.sleep(delay)
     print()
 
+# Function to return coloured text for terminal output
 def coloured(text, colour='green'):
     colours = {
         'red': '\033[91m',
@@ -22,7 +22,10 @@ def coloured(text, colour='green'):
         'cyan': '\033[96m',
         'magenta': '\033[38;5;125m',
         'blue': '\033[38;5;21m',
+        'yellow': '\033[38;5;222m',
         'end': '\033[0m'
     }
     return f"{colours.get(colour, '')}{text}{colours['end']}"
 
+def introduction():
+    animate(coloured())
