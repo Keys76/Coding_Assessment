9b6063720e7576a9398bf4215ec1bb985634549b
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-06 13:45:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from functions import clear_screen, cls, animate, coloured
 from data import cities
 cls
 clear_screen()
-animate(coloured("hellllooo there", "red"))
+animate(coloured("red text", "red"))
 clear_screen()
 
-print(cities)
+
 
