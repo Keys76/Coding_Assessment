bf1cbfb86aa4921f7e928127d89d9c63f6870a7a
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-07 06:07:01 +1000

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ animate(coloured(cities[5], "red"))
 # Wait for a second before clearing the screen
 time.sleep(1)
 clear_screen()
+
+# Display a message indicating the activation of the Time Beacon
+animate("Time Beacon 505 activated...")
+animate("loading data...")
+animate("complete")
+
+time.sleep(1)
+clear_screen()
+
+animate("message from commander:")
+print("Agent 0712, you are our last hope to save the timeline. " \
+    "There is something trapping the entire agency in a time loop." \
+    " You must track it down, following its path throughout time, and defeat it." \
+    " We are all counting on you to save us.")
