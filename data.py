ddaaa753598a48ff6f60c2efbb5fb52fdd295a14
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-06 16:04:16 +1000

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
+#list of cities with emojis and themes
 cities = [
     "🏛 Ancient Greece 📜",
     "⛩️ Ancient China 🐲",
     "🛕 Ancient India 🍛",
-    "🇲🇪 Ancient Rome 🕊️",
+    "⚔️ Ancient Rome 🕊️",
     "🐫 Ancient Egypt ⚱️",
     "🪓 Vikings ⛵"
 ]
