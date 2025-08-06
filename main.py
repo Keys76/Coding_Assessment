711927dcc71ab8707d68ca905114c4de847fc884
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-06 16:04:07 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from functions import clear_screen, cls, animate, coloured 
+import time
+from functions import clear_screen, animate, coloured 
 from data import cities
 
-cls
+# Clear the screen before starting the animation
 clear_screen()
 
-
+# Animate the cities with different colours
 animate(coloured(cities[0], "cyan"))
 animate(coloured(cities[1], "green"))
 animate(coloured(cities[2], "magenta"))
 animate(coloured(cities[3], "blue"))
-animate(colour)
+animate(coloured(cities[4], "yellow"))
+animate(coloured(cities[5], "red"))
+
+# Wait for a second before clearing the screen
+time.sleep(1)
+clear_screen()
