ada5e390773af265c9b145d7d4465b265c0fcca3
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-06 13:43:21 +1000

    coloured text

--- functions.py
+++ functions.py
@@ -2,3 +2,29 @@ import os
 
 def clear_screen():
     os.system ('cls' if os.name == 'nt' else 'clear')
+
+import os
+import sys
+import time
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def animate(text, delay=0.05):
+    for c in text:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
+
+def coloured(text, colour='green'):
+    colours = {
+        'red': '\033[91m',
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'cyan': '\033[96m',
+        'end': '\033[0m'
+    }
+    return f"{colours.get(colour, '')}{text}{colours['end']}"
+
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from functions import clear_screen
-
+from functions import clear_screen, cls, animate, coloured 
+from data import cities
+cls
 clear_screen()
+animate(coloured("hellllooo there", "red"))
+clear_screen()
+
+print(cities)
+
