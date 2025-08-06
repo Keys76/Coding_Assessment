4389c2556257327c9e60765783b2aad8c31c13d5
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-07 07:53:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import time
-from functions import clear_screen, animate, coloured 
+from functions import clear_screen, animate, coloured, clear_wait 
 from data import cities
 
 # Clear the screen before starting the animation
 clear_screen()
 
 # Animate the cities with different colours
-animate(coloured(cities[0], "cyan"))
-animate(coloured(cities[1], "green"))
-animate(coloured(cities[2], "magenta"))
-animate(coloured(cities[3], "blue"))
-animate(coloured(cities[4], "yellow"))
-animate(coloured(cities[5], "red"))
+#animate(coloured(cities[0], "cyan"))
+#animate(coloured(cities[1], "green"))
+#animate(coloured(cities[2], "magenta"))
+#animate(coloured(cities[3], "blue"))
+#animate(coloured(cities[4], "yellow"))
+#animate(coloured(cities[5], "red"))
 
 # Wait for a second before clearing the screen
-time.sleep(1)
-clear_screen()
+clear_wait()
 
 # Display a message indicating the activation of the Time Beacon
-animate("Time Beacon 505 activated...")
-animate("loading data...")
-animate("complete")
-
-time.sleep(1)
+animate(coloured("Time Beacon 505 activated...", "code"))
 clear_screen()
+animate(coloured("Loading data...", "code"))
+clear_screen()
+animate(coloured("Complete", "code"))
 
-animate("message from commander:")
-print("Agent 0712, you are our last hope to save the timeline. " \
-    "There is something trapping the entire agency in a time loop." \
-    " You must track it down, following its path throughout time, and defeat it." \
-    " We are all counting on you to save us.")
+clear_wait()
+
+animate(coloured("Message from commander:", "code"))
+print(coloured("Agent 0712, you are our last hope to save the timeline. There is something trapping the entire agency in a time loop. You must track it down, following its path throughout time, and defeat it.We are all counting on you to save us.", "code"))
+
