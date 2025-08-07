7d1a0f764288d746f07fd7fec5db9ded11ef9094
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-08 08:11:12 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -42,5 +42,8 @@ def introduction():
     animate(coloured("Complete", "code"))
     clear_wait()
     animate(coloured("Message from commander:", "code"))
-    print(coloured("Agent 0712, you are our last hope to save the timeline. There is something trapping the entire agency in a time loop. You must track it down, following its path throughout time, and defeat it.We are all counting on you to save us.", "code"))
+    print(coloured("Agent 0712, you are our last hope to save the timeline.\nThere is something trapping the entire agency in a time loop.\nYou must track it down, following its path throughout history, and defeat it.\nWe are all counting on you to save us.", "code"))
+    time.sleep(10)
+    clear_screen()
+
 
