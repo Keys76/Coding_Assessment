9210222e8bc4a250b58f90159a2cf8632f873ce8
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-08 08:11:09 +1000

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,23 @@
 #list of cities with emojis and themes
 cities = [
-    "🏛 Ancient Greece 📜",
-    "⛩️ Ancient China 🐲",
-    "🛕 Ancient India 🍛",
-    "⚔️ Ancient Rome 🕊️",
-    "🐫 Ancient Egypt ⚱️",
-    "🪓 Vikings ⛵"
+    "Ancient Greece",
+    "Ancient China",
+    "Ancient India",
+    "Ancient Rome",
+    "Ancient Egypt",
+    "Vikings"
 ]
 
+# Create variables for the city and date
+city = "Thebes"
+date = "429 BCE"
+health = 100
+
+items = [
+    "Time Beacon 505",
+    "Bottle of Water",
+    "First Aid Kit",
+    "Flashlight",
+    "Map",
+    "One set of rations"
+]
