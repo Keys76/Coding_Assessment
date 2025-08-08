01a83bbda682136289b306fafad3aa48d7364795
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-08 11:07:47 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import time
-from functions import clear_screen, animate, coloured, clear_wait, introduction 
-from data import cities, city, date, health, items
+from functions import clear_screen, animate, coloured, clear_wait, introduction, stats 
+from data import cities, city, date, health, items, missions
 
 # Clear the screen before starting the animation
 clear_screen()
-
-# Animate the cities with different colours
-#animate(coloured(cities[0], "cyan"))
-#animate(coloured(cities[1], "green"))
-#animate(coloured(cities[2], "magenta"))
-#animate(coloured(cities[3], "blue"))
-#animate(coloured(cities[4], "yellow"))
-#animate(coloured(cities[5], "red"))
 
 # Wait for a second before clearing the screen
 clear_wait()
@@ -19,10 +11,12 @@ clear_wait()
 # Display the introduction message
 introduction()
 
-animate(coloured("Agent Information:", "code"))
-print(coloured(f"\nLocation: {cities[0]}", "cyan"))
-print(coloured(f"City: {city}", "cyan"))
-print(coloured(f"Date: {date}", "cyan"))
-print(coloured(f"Health: {health}", "cyan"))
-print(coloured("\nItems:", "code"))
+stats()
+
+
+
+
+
+
+
 
