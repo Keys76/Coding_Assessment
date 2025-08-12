5d2e2b1a753cbd2fa7a7f62d0a9ffa76f7440edb
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-12 13:11:12 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-from data import cities, city, date, health, items, missions  
+from data import civilisations, city, date, health, items, missions  
 
 # Function to clear the console screen
 def clear_screen():
@@ -51,7 +51,7 @@ def introduction():
 # Function to display agent stats and mission details
 def stats():
     animate(coloured("Agent Information:", "bold"))
-    print(coloured(f"\nLocation: {cities[0]}", "cyan"))
+    print(coloured(f"\nLocation: {civilisations[0]}", "cyan"))
     print(coloured(f"City: {city}", "cyan"))
     print(coloured(f"Date: {date}", "cyan"))
     print(coloured(f"Health: {health}", "cyan"))
