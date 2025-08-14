fb4c310d06db038a0ee7fdd8a8660c14d65c2bc3
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-15 07:51:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from functions import clear_screen, animate, coloured, clear_wait, introduction, stats 
-from data import cities, city, date, health, items, missions
+from data import cities, civilisations, date, health, items, missions
 
 # Clear the screen before starting the animation
 clear_screen()
@@ -12,8 +12,6 @@ clear_wait()
 introduction()
 
 stats()
-
-
 
 
 
