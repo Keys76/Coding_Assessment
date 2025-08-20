315ab5af81f9289694c3d7725792e64613000898
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-20 15:10:18 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,31 @@ from data import *
 clear_screen()
 
 # Wait for a second before clearing the screen
-clear_wait()
+print(coloured("                       ","back"))
 
+print(coloured("        ","back"),coloured("     ","black_back"),coloured("       ","back"))
+print(coloured("   ","back"),coloured("   ","black_back"),coloured("     ","testicles"),coloured("   ","black_back"),coloured("  ","back"))
+
+print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
+print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
+print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
+print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
+
+print(coloured("  ","back"),coloured("  ","black_back"),coloured("         ","testicles"),coloured(" ","black_back"),coloured("  ","back"))
+print(coloured("  ","back"),coloured("  ","black_back"),coloured("         ","testicles"),coloured(" ","black_back"),coloured("  ","back"))
+
+print(coloured("     ","back"),coloured(" ","black_back"),coloured("     ","testicles"),coloured(" ","black_back"),coloured("    ","back"))
+print(coloured("      ","back"),coloured(" ","black_back"),coloured("   ","testicles"),coloured(" ","black_back"),coloured("     ","back"))
+print(coloured("         ","back"),coloured("   ","black_back"),coloured("        ","back"))
+
+print(coloured("                       ","back"))
+
+time.sleep(10000)
 # Display the introduction message
 introduction()
 
 # Display the agent stats and mission details
 stats()
-
-print(coloured(".                              "))
-
-
-
 
 
 
