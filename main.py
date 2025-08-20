20736eccafecaeb5dfb0b57624a22ac5aeed13ab
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-21 07:40:11 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import time
 from functions import *
 from data import *
 number = 1
+health = 100
 
 # Clear the screen before starting the animation
 clear_screen()
 
 # Wait for a second before clearing the screen
 clear_wait()
-        
+
 # Display the introduction message
 introduction()
 
 # Display the agent stats and mission details
-statssss(number)
+stats(number, health)
+
+# Prompt the user to continue the game
+continue_game()
+
 
 
 
