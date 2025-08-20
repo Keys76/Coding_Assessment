8223a23bd5cccdd1604eabe78369eca3797c48da
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-20 20:22:31 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,32 @@ from data import *
 clear_screen()
 
 # Wait for a second before clearing the screen
-print(coloured("                       ","back"))
+print(coloured("                         ","back"))
 
-print(coloured("        ","back"),coloured("     ","black_back"),coloured("       ","back"))
-print(coloured("   ","back"),coloured("   ","black_back"),coloured("     ","testicles"),coloured("   ","black_back"),coloured("  ","back"))
+print(coloured("         ","back"),coloured("     ","black_back"),coloured("         ","back"))
+print(coloured("    ","back"),coloured("   ","black_back"),coloured("       ","t"),coloured("   ","black_back"),coloured("    ","back"))
 
-print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
-print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
-print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
-print(coloured(" ","back"),coloured(" ","black_back"),coloured("               ","testicles"),coloured(" ","black_back"),coloured(" ","back"))
+print(coloured("  ","back"),coloured(" ","black_back"),coloured("               ","t"),coloured(" ","black_back"),coloured("  ","back"))
+print(coloured("  ","back"),coloured(" ","black_back"),coloured("               ","t"),coloured(" ","black_back"),coloured("  ","back"))
+print(coloured("  ","back"),coloured(" ","black_back"),coloured("               ","t"),coloured(" ","black_back"),coloured("  ","back"))
+print(coloured("  ","back"),coloured(" ","black_back"),coloured("               ","t"),coloured(" ","black_back"),coloured("  ","back"))
 
-print(coloured("  ","back"),coloured("  ","black_back"),coloured("         ","testicles"),coloured(" ","black_back"),coloured("  ","back"))
-print(coloured("  ","back"),coloured("  ","black_back"),coloured("         ","testicles"),coloured(" ","black_back"),coloured("  ","back"))
+print(coloured("    ","back"),coloured(" ","black_back"),coloured("           ","t"),coloured(" ","black_back"),coloured("    ","back"))
+print(coloured("    ","back"),coloured(" ","black_back"),coloured("           ","t"),coloured(" ","black_back"),coloured("    ","back"))
 
-print(coloured("     ","back"),coloured(" ","black_back"),coloured("     ","testicles"),coloured(" ","black_back"),coloured("    ","back"))
-print(coloured("      ","back"),coloured(" ","black_back"),coloured("   ","testicles"),coloured(" ","black_back"),coloured("     ","back"))
-print(coloured("         ","back"),coloured("   ","black_back"),coloured("        ","back"))
+print(coloured("      ","back"),coloured(" ","black_back"),coloured(" ","shadow"),coloured("      ","t"),coloured(" ","black_back"),coloured("     ","back"))
+print(coloured("       ","back"),coloured(" ","black_back"),coloured("     ","shadow"),coloured(" ","black_back"),coloured("       ","back"))
 
-print(coloured("                       ","back"))
+print(coloured("          ","back"),coloured("   ","black_back"),coloured("          ","back"))
+print(coloured("                         ","back"))
 
-time.sleep(10000)
+time.sleep(10000000)
+clear_wait()
+        
 # Display the introduction message
 introduction()
+
+number = 1
 
 # Display the agent stats and mission details
 stats()
