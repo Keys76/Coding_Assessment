ed513ffb5fc09e8c5ae057881e5c2dd14b96e1b4
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-25 07:31:32 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ import os
 import sys
 import time
 from data import * 
-from main import alive, game_loop
 
 # Function to clear the console screen
 def clear_screen():
@@ -178,34 +177,7 @@ def menu():
     return input_menu
 
 
-def main():
-    while True:
-        input_menu = menu()
-        if input_menu == "start":
-            clear_screen()
-            if alive == True:
-                game_loop()
-            else:
-                clear_screen()
-                print(coloured("GAME OVER","red"))
-                main() 
-        elif input_menu == "instructions":
-            clear_screen()
-            print(coloured("--------------- INSTRUCTIONS ---------------","bold"))
-            animate(coloured("\nObjective:","bold"))
-            print(coloured("\n. Travel through different ancient civilisations to complete missions and restore the timeline.","code"))
-            print(coloured(". Solve puzzles, make choices, and overcome challenges to progress.","code"))
-            animate(coloured("\nControls:","bold"))
-            print(coloured("\n. Input your choices by typing the corresponding number or word and pressing Enter.","code"))
-            print(coloured(". Follow on-screen prompts to navigate through the game.","code"))
-            animate(coloured("\nGameplay Tips:","bold"))
-            print(coloured("\n. Be careful with choices — some paths may end your journey","code"))
-            print(coloured(". Completing missions in each civilization repairs the timeline","code"))
-            continue_game()
-            main()
-        else:
-            clear_screen()
-            main()
+
 
 
 
