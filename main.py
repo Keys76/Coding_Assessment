e5c3d16b2f59bd8802fe7cdefa8acd788c5d89d1
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-25 21:15:25 +1000

    Update main.py

--- main.py
+++ main.py
@@ -51,87 +51,92 @@ def main():
             menu(civilisation_number)
 
 def game_loop(civilisation_number, agent):
-    print("Level" , civilisation_number)
-    riddle_attempts = 0
-    time.sleep(2)
 
-    # Clear the screen before starting the animation
-    clear_screen()
+    if civilisation_number == 1:
 
-    # Wait for a second before clearing the screen
-    clear_wait()
+        riddle_attempts = 0
+        time.sleep(2)
 
-    # Display the introduction message
-    introduction()
+        # Clear the screen before starting the animation
+        clear_screen()
 
-    # Display the agent stats and mission details
-    stats(civilisation_number, agent)
+        # Wait for a second before clearing the screen
+        clear_wait()
 
-    # Prompt the user to continue the game
-    continue_game()
+        # Display the introduction message
+        introduction()
 
-    # First Mission - Ancient Greece
-    print(coloured("🏛  Ancient Greece 🔱", "bold"))
-    print()
-    print(coloured(
-        "You look around and see tall stone walls and crowded houses behind them. The streets look quiet, with fewer people than you would expect for a big city. "
-        "\nMany shops are closed, and travelers seem nervous. Outside the gates, the Sphinx waits, keeping trade and visitors away.","cyan"
-        ))
-    print()
-    print(coloured(
-        "As you leave the quiet streets and walk toward the gates, the Sphinx comes into view. She sits on a rock by the road, her sharp eyes fixed on you. "
-        "\nRising to block the way, she spreads her wings and declares: Answer my riddle, or you shall not pass.","cyan"
-    ))
-    continue_game()
-    print(coloured("🏛  Ancient Greece 🔱", "bold"))
-    print()
-    print(coloured(
-        "The Sphinx's voice is low and steady: 'What walks on four legs in the morning, two legs at noon, and three legs in the evening?'"
-        "\nShe watches you closely, waiting for your answer.","cyan"
-    ))
-    print(coloured("\nYou have 4 choices, choose wisely as it will determine your fate:", "bold"))
-    animate(coloured("\n1: Dog", "cyan"))
-    animate(coloured("2: Human", "cyan"))
-    animate(coloured("3: Old Tree", "cyan"))
-    animate(coloured("4: Bird", "cyan"))
-    riddle_answer =int(input(coloured("\nType the number that corresponds to your answer and press enter...", "bold")))
+        # Display the agent stats and mission details
+        stats(civilisation_number, agent)
 
-    # Loop to give different outcomes based on the user's answer to the riddle
-    while riddle_attempts < 1:
-        # Check the user's answer and provide the corresponding outcome
-        if riddle_answer == 2:
-            clear_wait()
-            print(coloured("🏛  Ancient Greece 🔱","bold"))
-            animate(coloured("\nThe Sphinx's eyes narrow, then she lets out a piercing cry. With a furious beat of her wings, she leaps from the rock and flies away into the mountains."
-            "\nThe people of Thebes are freed at last","cyan"))
-            continue_game()
-            riddle_attempts = riddle_attempts + 1
-            return "win", agent
-        # If the answer is incorrect, provide a different outcome
-        elif riddle_answer == 1 or riddle_answer == 3 or riddle_answer == 4:
-            clear_wait()
-            print(coloured("🏛  Ancient Greece 🔱","bold"))
-            animate(coloured("\nThe Sphinx's lips curl into a cruel smile. “Wrong,” she hisses, stepping closer. Her wings spread wide as the ground trembles with her power.","cyan"))
-            animate(coloured("The air fills with dread. With a swift motion, she strikes you down. Darkness engulfs you as you fall to the ground...","cyan"))
-            riddle_attempts = riddle_attempts + 1
-            return "dead", agent
-        # If the input is invalid, prompt the user to enter a valid number
-        else:
-            animate(coloured("\nInvalid input. Please enter a number between 1 and 4.", "red"))
-            clear_wait()
-            print(coloured("🏛  Ancient Greece 🔱", "bold"))
-            print()
-            print(coloured(
-                "The Sphinx's voice is low and steady: 'What walks on four legs in the morning, two legs at noon, and three legs in the evening?'"
-                "\nShe watches you closely, waiting for your answer.","cyan"
+        # Prompt the user to continue the game
+        continue_game()
+
+        # First Mission - Ancient Greece
+        print(coloured("🏛  Ancient Greece 🔱", "bold"))
+        print()
+        print(coloured(
+            "You look around and see tall stone walls and crowded houses behind them. The streets look quiet, with fewer people than you would expect for a big city. "
+            "\nMany shops are closed, and travelers seem nervous. Outside the gates, the Sphinx waits, keeping trade and visitors away.","cyan"
             ))
-            print(coloured("\nYou have 4 choices, choose wisely as it will determine your fate:", "bold"))
-            print(coloured("\n1: Dog", "cyan"))
-            print(coloured("2: Human", "cyan"))
-            print(coloured("3: Old Tree", "cyan"))
-            print(coloured("4: Bird", "cyan"))
-            riddle_answer =int(input(coloured("\nType the number that corresponds to your answer and press enter...", "bold")))
-    
+        print()
+        print(coloured(
+            "As you leave the quiet streets and walk toward the gates, the Sphinx comes into view. She sits on a rock by the road, her sharp eyes fixed on you. "
+            "\nRising to block the way, she spreads her wings and declares: Answer my riddle, or you shall not pass.","cyan"
+        ))
+        continue_game()
+        print(coloured("🏛  Ancient Greece 🔱", "bold"))
+        print()
+        print(coloured(
+            "The Sphinx's voice is low and steady: 'What walks on four legs in the morning, two legs at noon, and three legs in the evening?'"
+            "\nShe watches you closely, waiting for your answer.","cyan"
+        ))
+        print(coloured("\nYou have 4 choices, choose wisely as it will determine your fate:", "bold"))
+        animate(coloured("\n1: Dog", "cyan"))
+        animate(coloured("2: Human", "cyan"))
+        animate(coloured("3: Old Tree", "cyan"))
+        animate(coloured("4: Bird", "cyan"))
+        riddle_answer =int(input(coloured("\nType the number that corresponds to your answer and press enter...", "bold")))
+
+        # Loop to give different outcomes based on the user's answer to the riddle
+        while riddle_attempts < 1:
+            # Check the user's answer and provide the corresponding outcome
+            if riddle_answer == 2:
+                clear_wait()
+                print(coloured("🏛  Ancient Greece 🔱","bold"))
+                animate(coloured("\nThe Sphinx's eyes narrow, then she lets out a piercing cry. With a furious beat of her wings, she leaps from the rock and flies away into the mountains."
+                "\nThe people of Thebes are freed at last","cyan"))
+                continue_game()
+                riddle_attempts = riddle_attempts + 1
+                return "win", agent
+            # If the answer is incorrect, provide a different outcome
+            elif riddle_answer == 1 or riddle_answer == 3 or riddle_answer == 4:
+                clear_wait()
+                print(coloured("🏛  Ancient Greece 🔱","bold"))
+                animate(coloured("\nThe Sphinx's lips curl into a cruel smile. “Wrong,” she hisses, stepping closer. Her wings spread wide as the ground trembles with her power.","cyan"))
+                animate(coloured("The air fills with dread. With a swift motion, she strikes you down. Darkness engulfs you as you fall to the ground...","cyan"))
+                riddle_attempts = riddle_attempts + 1
+                return "dead", agent
+            # If the input is invalid, prompt the user to enter a valid number
+            else:
+                animate(coloured("\nInvalid input. Please enter a number between 1 and 4.", "red"))
+                clear_wait()
+                print(coloured("🏛  Ancient Greece 🔱", "bold"))
+                print()
+                print(coloured(
+                    "The Sphinx's voice is low and steady: 'What walks on four legs in the morning, two legs at noon, and three legs in the evening?'"
+                    "\nShe watches you closely, waiting for your answer.","cyan"
+                ))
+                print(coloured("\nYou have 4 choices, choose wisely as it will determine your fate:", "bold"))
+                print(coloured("\n1: Dog", "cyan"))
+                print(coloured("2: Human", "cyan"))
+                print(coloured("3: Old Tree", "cyan"))
+                print(coloured("4: Bird", "cyan"))
+                riddle_answer =int(input(coloured("\nType the number that corresponds to your answer and press enter...", "bold")))
+
+
+plant()
+time.sleep(200000)
 main()
 
 
