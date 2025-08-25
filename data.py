c6552a5d23ea3a61eb73c2c4ff8070fcf47927e4
Keys76 <keira.francis@education.nsw.gov.au>
2025-08-25 20:49:55 +1000

    Update data.py

--- data.py
+++ data.py
@@ -1,33 +1,12 @@
-from functions import *
-# List of civilisations in the game
-civilisations = [
-    "Ancient Greece",
-    "Ancient China",
-    "Ancient India",
-    "Ancient Rome",
-    "Ancient Egypt",
-    "Vikings",
-]
 
-# List of cities, dates, items, and missions for the game
-cities = [
-    "Thebes",
-    "Yangtze River Basin",
-    "Palitaputra",
-    "Rome",
-    "Giza",
-    "Norway",
-]
-
-# Dates corresponding to the cities
-date = [
-    "429 BCE",
-    "3001 BCE",
-    "1100 CE",
-    "352 CE",
-    "2576 CE",
-    "999 CE"
-]
+civilisations_dict = {
+    1: {"civilisation": "Ancient Greece", "city": "Thebes", "date": "429 BCE", "mission": "Find the sphinx who is reigning terror over the city of Thebes, you must answer its riddle to pass."},
+    2: {"civilisation": "Ancient China", "city": "Yangtze River Basin", "date": "3001 BCE", "mission": "mission 2"},
+    3: {"civilisation": "Ancient India", "city": "Palitaputra", "date": "1100 CE", "mission": "mission 3"},
+    4: {"civilisation": "Ancient Rome", "city": "Rome", "date": "352 CE", "mission": "mision 4"},
+    5: {"civilisation": "Ancient Egypt", "city": "Giza", "date": "2576 CE", "mission": "mission 5"},
+    6: {"civilisation": "Vikings", "city": "Norway", "date": "999 CE", "mission": "mission 6"},
+} 
 
 # List of items available in the game
 items = [
@@ -37,15 +16,5 @@ items = [
     "Flashlight",
     "Map",
     "One set of rations"
-]
-
-# List of missions corresponding to the cities
-missions = [
-    "Find the sphinx who is reigning terror over the city of Thebes, you must answer its riddle to pass.",
-    "mission 2",
-    "mission 3",
-    "mision 4",
-    "mission 5",
-    "mission 6"
 ]
 
